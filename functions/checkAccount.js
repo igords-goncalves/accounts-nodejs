ee5a600d85cb396f48b4076541bb7117613bc5c6
Igor Gonçalves <igordossantos38@gmail.com>
2022-11-04 09:17:15 -0300

    fix a return statement
    Just add a return on function

--- functions/checkAccount.js
+++ functions/checkAccount.js
@@ -2,7 +2,7 @@ const chalk = require("chalk");
 const fs = require("fs");
 
 function checkAccount(nomeDaConta) {
-    if (!fs.existsSync("db_contas")) fs.mkdirSync("db_contas");
+    if (!fs.existsSync("db_contas")) return fs.mkdirSync("db_contas");
 
     if (fs.existsSync(`db_contas/${nomeDaConta}.json`)) {
         console.log(chalk.bgRed.black("Conta já existe, escolha outro nome."));
