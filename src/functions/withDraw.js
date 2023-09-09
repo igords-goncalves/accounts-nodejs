496e81a8cbb2d167865d3db88efc71a77ef0a69b
Igor Gonçalves <igordossantos38@gmail.com>
2023-09-09 01:08:46 -0300

    feat: create withDraw functionality

---
+++ src/functions/withDraw.js
@@ -0,0 +1,60 @@
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const getBalance = require("./getBalance");
+
+function withDraw() {
+    inquirer
+        .prompt([
+            {
+                name: "acountName",
+                message: chalk.yellow("Digite o nome da conta:"),
+            },
+            {
+                name: "value",
+                message: chalk.bgRedBright.black(
+                    "Digite o valor do seu saque:"
+                ),
+            },
+        ])
+        .then((resp) => {
+            const acountName = resp["acountName"];
+            const capitalizedName = acountName.toUpperCase();
+
+            fs.readFile(
+                `./db_contas/${capitalizedName}.json`,
+                "utf8",
+                (err, data) => {
+                    if (err) {
+                        console.log(chalk.red("Conta inexistente"));
+                        exit();
+                        return;
+                    }
+
+                    const acountDatas = JSON.parse(data);
+                    acountDatas.saldo -= parseFloat(
+                        resp["value"].replace(",", ".")
+                    );
+                    const newBalance = JSON.stringify(acountDatas, null, 2);
+
+                    fs.writeFile(
+                        `./db_contas/${capitalizedName}.json`,
+                        newBalance,
+                        "utf8",
+                        (err) => {
+                            if (err) {
+                                console.log("Algo deu errado");
+                            } else {
+                                getBalance();
+                            }
+                        }
+                    );
+                }
+            );
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+module.exports = withDraw;
