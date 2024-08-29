a24fed8162c80e8c4d6840a23e5beca130bd37d0
Igor Gonçalves <igordossantos38@gmail.com>
2024-08-29 17:01:19 -0300

    refactor: update setOperation function to use constant for menu options

--- src/functions/setOperation.js
+++ src/functions/setOperation.js
@@ -1,47 +1,44 @@
 const inquirer = require("inquirer");
-const exit = require("./exit.js");
 
 const displayMessage = require("./displayMessage.js");
 const getBalance = require("./getBalance.js");
 const deposit = require("./deposit.js");
 const withDraw = require("./withDraw.js");
+const exit = require("./exit.js");
+
+const OPTIONS = require("../constants/options.js");
 
 function setOperation() {
-    const options = [
-        "Criar Conta",
-        "Consultar Saldo",
-        "Depositar",
-        "Sacar",
-        "Sair",
-    ];
-    console.clear();
-    inquirer
-        .prompt({
-            type: "list",
-            name: "action",
-            message: "Escolha o serviço:",
-            choices: options,
-        })
-        .then((resp) => {
-            const action = resp["action"];
+  // Limpa o console após a execução
+  console.clear();
 
-            switch (action) {
-                case options[0]:
-                    displayMessage();
-                    break;
-                case options[1]:
-                    getBalance();
-                    break;
-                case options[2]:
-                    deposit();
-                    break;
-                case options[3]:
-                    withDraw();
-                    break;
-                default:
-                    exit();
-            }
-        });
+  return inquirer
+    .prompt({
+      type: "list",
+      name: "action",
+      message: "Escolha o serviço:",
+      choices: OPTIONS,
+    })
+    .then((resp) => {
+      const action = resp["action"];
+
+      switch (action) {
+        case OPTIONS[0]:
+          displayMessage();
+          break;
+        case OPTIONS[1]:
+          getBalance();
+          break;
+        case OPTIONS[2]:
+          deposit();
+          break;
+        case OPTIONS[3]:
+          withDraw();
+          break;
+        default:
+          exit();
+      }
+    });
 }
 
 module.exports = setOperation;
